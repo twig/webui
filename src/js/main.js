95eb18270e0ab43fcef0c1d4a2e907469f47f8ed
Twig N <koonkii@hotmail.com>
2020-02-26 21:14:25 +1100

    - updated g_winTitle with unicode string

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ Copyright (c) 2011 BitTorrent, Inc. All rights reserved.
 Use of this source code is governed by a BSD-style that can be
 found in the LICENSE file.
 */
-var g_winTitle = "\u00B5Torrent WebUI v" + CONST.VERSION;
+var g_winTitle = "µTorrent WebUI v" + CONST.VERSION;
 
 // Localized string globals ... initialized in loadLangStrings()
 
