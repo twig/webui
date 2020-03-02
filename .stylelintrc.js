ce5c63ebfdbb0a1188b09fc655cb0067101b7fda
Twig N <koonkii@hotmail.com>
2020-03-02 20:58:06 +1100

    - ignore selector-max-id

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -10,6 +10,7 @@ module.exports = {
     // ignore these
     "comment-empty-line-before": null,
     "order/properties-alphabetical-order": null,
+    "selector-max-id": null,
 
     // customise rules
     "selector-no-qualifying-type": [true, { ignore: ["attribute", "class"] }],
@@ -17,7 +18,6 @@ module.exports = {
     // ignoring for now
     "selector-class-pattern": null,
     "no-descending-specificity": null,
-    "selector-max-id": null,
     "shorthand-property-no-redundant-values": null,
     "comment-whitespace-inside": null,
     "selector-max-compound-selectors": null,
